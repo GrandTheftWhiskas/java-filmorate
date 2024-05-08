144d3bca1294dfd2781b6d3658b60ac3ee019492
Эдуард <example@gmail.com>
2024-05-08 14:42:40 +0300

    ТЗ 10-го спринта. Фикс

--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -14,6 +14,7 @@ import java.time.Month;
 class FilmorateApplicationTests {
     FilmController filmController = new FilmController();
 	UserController userController = new UserController();
+
 	@Test
 	void filmAddTest() {
 		Film film = new Film("Титаник", "Романтическая драма и фильм-катастрофа",
